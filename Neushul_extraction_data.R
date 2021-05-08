6f6a5fc1054e6973c1de7b6241a1b7a508a0eb5e
Ross Whippo <rosswhippo@gmail.com>
2021-05-08 10:54:06 -0700

    Added data files and started analysis scripts

---
+++ Neushul_extraction_data.R
@@ -0,0 +1,76 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                                                                                ##
+# Neushul Algal Survey Redux - Extraction Data                                   ##
+# Data are current as of 2021-05-07                                              ##
+# Data source: M. Neushul, Studies of Subtidal Marine Vegetation in Western      ##  
+#                   Washington, Ecology, Vol. 48, No. 1 (Jan., 1967), pp. 83-94  ##
+# R code prepared by Ross Whippo                                                 ##
+# Last updated 2021-05-07                                                        ##
+#                                                                                ##
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# SUMMARY: Analysis of data from algal survey in San Juan Islands from 1967.
+
+
+# Required Files (check that script is loading latest version):
+# Neushul_Table_1.csv
+# Neushul_Xsection_data-extractions.csv
+# NeushulSiteDetailsPreplan.csv
+
+# Associated Scripts:
+# none
+
+# TO DO 
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# TABLE OF CONTENTS                                                            ####
+#                                                                                 +
+# RECENT CHANGES TO SCRIPT                                                        +
+# LOAD PACKAGES                                                                   +
+# READ IN AND PREPARE DATA                                                        +
+# MANIPULATE DATA                                                                 +
+#                                                                                 +
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# RECENT CHANGES TO SCRIPT                                                     ####
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# 2021-05-07 Script created
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# LOAD PACKAGES                                                                ####
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+library(tidyverse)
+library(algaeClassify)
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# READ IN AND PREPARE DATA                                                     ####
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# site details extracted
+site_deets <- read_csv("data/extraction/NeushulSiteDetailsPreplan.csv")
+
+# data from table 1
+Table_1 <- read_csv("data/extraction/Neushul_Table_1.csv")
+
+# data from cross-section figures
+x_sections <- read_csv("data/extraction/Neushul_Xsection_data-extractions.csv")
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# MANIPULATE DATA                                                              ####
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# extract list of genus/species from table 1 and check against database
+algae_list <- spp_list_algaebase(Table_1, phyto.name = 'Name', lakename = "",
+                   long = FALSE, write = FALSE)
+
+############### SUBSECTION HERE
+
+####
+#<<<<<<<<<<<<<<<<<<<<<<<<<<END OF SCRIPT>>>>>>>>>>>>>>>>>>>>>>>>#
+
+# SCRATCH PAD ####
